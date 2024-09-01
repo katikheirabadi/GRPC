eb29618970aa2ddaf0ccd125ecc581afcd41ff4b
kati kheirabadi <kati.kheirabadi@gmail.com>
2024-09-01 22:44:25 +0330

    linq to proto send request

--- GRPC.Client/Program.cs
+++ GRPC.Client/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using Grpc.Net.Client;
+using GRPC.Server.Protos;
+
+using (var service = GrpcChannel.ForAddress("https://localhost:7197/"))
+{
+    var client = new SumService.SumServiceClient(service);
+    var req = new SumRequest
+    {
+        Num1 = 10,
+        Num2 = 20
+    };
+    var result = client.Sum(req);
+    Console.WriteLine(result);
+    Console.ReadKey();
+}
